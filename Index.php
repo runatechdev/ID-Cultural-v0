a82cc602a46eaa752fc881e068098362856b297b
Maximiliano Padilla <limonsinazucar@gmail.com>
2025-07-15 02:59:48 -0300

    Fix stylesheet path in Index.php
    Updated the href for main.css to use an absolute path, ensuring the stylesheet loads correctly regardless of the current URL.

--- Index.php
+++ Index.php
@@ -7,7 +7,7 @@
 
   <title>DNI Cultural</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-  <link rel="stylesheet" href="static/css/main.css" />
+  <link rel="stylesheet" href="/static/css/main.css"/>
 
 </head>
 
